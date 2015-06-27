a4287a5a4ac783534fd4e3254030b84b119cc22c
ebiallergy <tamagoj@tobita-no-MacBook-Air.local>
2015-06-27 22:39:47 +0900

    ローディング画面作成

--- common/js/run.js
+++ common/js/run.js
@@ -1,14 +1,16 @@
-
-window.onload = function(){
-    $(function() {
-        $('.loading').addClass('loading-on');
-    });
-}
-
 $(function(){
+    //js-on
+    $('body').removeClass('js-off');
+    
     //pageTop
     $('.pageTop').on('click',function(e){
         e.preventDefault();
         $('html, body').animate({scrollTop : 0},600, 'swing');
     });
 });
+
+window.onload = function(){
+    $(function() {
+        $('.loading').addClass('loading-on');
+    });
+}

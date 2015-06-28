4216fd99bbbca0f2543aa554bf1eab58fb02f250
ebiallergy <tamagoj@tobita-no-MacBook-Air.local>
2015-06-28 16:28:54 +0900

    スマホ時メニュー実装

--- common/js/run.js
+++ common/js/run.js
@@ -2,6 +2,20 @@ $(function(){
     //js-on
     $('body').removeClass('js-off');
     
+    //header-menu
+    var gNavi = $('.g-nav'),
+        naviMenu = $('.header-menu');
+    
+    naviMenu.on('click', function(){
+        if($(this).hasClass('header-menu-on')){
+            $(this).removeClass('header-menu-on');
+            $(gNavi).removeClass('g-nav-on');
+        } else {
+            $(this).addClass('header-menu-on');
+            $(gNavi).addClass('g-nav-on');
+        }
+    });
+    
     //pageTop
     $('.pageTop').on('click',function(e){
         e.preventDefault();
